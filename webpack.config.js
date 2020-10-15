604c8fa0ec5550b2912e8734682c820e042433ae
Pedro Gomes <pedrogomes94@gmail.com>
2020-10-15 13:55:16 +0200

    update umd name

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist', 'umd'),
     filename: '[name].min.js',
     libraryTarget: 'umd',
-    library: 'encUtils',
+    library: 'safeJsonUtils',
     umdNamedDefine: true,
     globalObject: 'this',
   },
